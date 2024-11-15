d8b2b19e824cc763b91c92b17bcf7569d59c5068
vandersonbfs <vanderson_b@hotmail.com>
2024-11-15 13:34:24 -0300

    feito a exclusão de usuarios

--- listar-usuario.php
+++ listar-usuario.php
@@ -9,27 +9,27 @@ $qtd = $res->num_rows;
 if ($qtd > 0) {
     print "<table class='table table-hover table-striped table-bordered'>";
     print "<tr>";
-        print "<th>id</th>";
-        print "<th>Nome</th>";
-        print "<th>Email</th>";
-        print "<th>Data de Nascimento</th>";
-        print "<th>Ações</th>";
-        print "<tr>";
+    print "<th>id</th>";
+    print "<th>Nome</th>";
+    print "<th>Email</th>";
+    print "<th>Data de Nascimento</th>";
+    print "<th>Ações</th>";
+    print "<tr>";
     while ($row = $res->fetch_object()) {
         print "<tr>";
-        print "<td>".$row->id."</td>";
-        print "<td>".$row->nome."</td>";
-        print "<td>".$row->email."</td>";
-        print "<td>".$row->data_nasc."</td>";
+        print "<td>" . $row->id . "</td>";
+        print "<td>" . $row->nome . "</td>";
+        print "<td>" . $row->email . "</td>";
+        print "<td>" . $row->data_nasc . "</td>";
         print "<td>
-            <button onclick=\"location.href='?page=editar&id=".$row->id."'\" class='btn btn-success'>Editar</button>
-            <button class='btn btn-danger'>Deletar</button>
+            <button onclick=\"location.href='?page=editar&id=" . $row->id . "'\" class='btn btn-success'>Editar</button>
+            <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar&acao=excluir&id=" . $row->id . "';}else{false;}\"class='btn btn-danger'>Deletar</button>
         </td>";
         print "<tr>";
     }
     print "<table>";
-}else{
-    print"<p class='alert alert-danger'>Ops... Não há registros.</p>";
+} else {
+    print "<p class='alert alert-danger'>Ops... Não há registros.</p>";
 }
 
 ?>
--- salvar-usuario.php
+++ salvar-usuario.php
@@ -44,7 +44,17 @@ switch ($_REQUEST["acao"]) {
         }
         break;
     case 'excluir':
-        # code...
+        $sql = "DELETE FROM usuarios WHERE id=".$_REQUEST["id"];
+        
+        $res  = $conn->query($sql);
+        if ($res==true) {
+            print "<script>alert('Excluído com sucesso');</script>";
+            print "<script>location.href='?page=listar';</script>";
+        }else{
+            print "<script>alert('Não foi possível excluir!');</script>";
+            print "<script>location.href='?page=listar';</script>";
+        }
+
         break;
 
     default:
