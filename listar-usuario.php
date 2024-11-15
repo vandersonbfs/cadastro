1988a29119e4766df238664a67c17e3fc7122794
vandersonbfs <vanderson_b@hotmail.com>
2024-11-15 11:46:19 -0300

    tabela de usuarios

--- listar-usuario.php
+++ listar-usuario.php
@@ -1,1 +1,35 @@
 <h1>listar usuário!</h1>
+<?php
+$sql = "SELECT * FROM usuarios";
+
+$res = $conn->query($sql);
+
+$qtd = $res->num_rows;
+
+if ($qtd > 0) {
+    print "<table class='table table-hover table-striped table-bordered'>";
+    print "<tr>";
+        print "<th>id</th>";
+        print "<th>Nome</th>";
+        print "<th>Email</th>";
+        print "<th>Data de Nascimento</th>";
+        print "<th>Ações</th>";
+        print "<tr>";
+    while ($row = $res->fetch_object()) {
+        print "<tr>";
+        print "<td>".$row->id."</td>";
+        print "<td>".$row->nome."</td>";
+        print "<td>".$row->email."</td>";
+        print "<td>".$row->data_nasc."</td>";
+        print "<td>
+            <button onclick=\"location.href='?page=editar&id=".$row->id."'\" class='btn btn-success'>Editar</button>
+            <button class='btn btn-danger'>Deletar</button>
+        </td>";
+        print "<tr>";
+    }
+    print "<table>";
+}else{
+    print"<p class='alert alert-danger'>Ops... Não há registros.</p>";
+}
+
+?>
